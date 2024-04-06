5ce47c4a59a177b4b18c5325cae4dea66524db88
맹주영 <mjy9088@naver.com>
2024-04-06 22:14:33 +0900

    add sample usage

--- sample/01-usage/main.ts
+++ sample/01-usage/main.ts
@@ -2,12 +2,9 @@ import { Primitive } from "../../src/main";
 import { Fake } from "../00-common/Fake";
 import { TypeIs } from "../00-common/TypeIs";
 import { Article } from "./model/Article";
-import { Comment } from "./model/Comment";
 import { User } from "./model/User";
 
 const User = Fake<User>();
-const Article = Fake<Article>();
-const Comment = Fake<Comment>();
 
 const userPrimitive = User(true);
 TypeIs<typeof userPrimitive, Primitive<User>>("same");
@@ -17,3 +14,31 @@ const userPrimitiveAll = User({
 TypeIs<typeof userPrimitiveAll, Primitive<User>>("same");
 const userEmptyInclude = User({ include: {} });
 TypeIs<typeof userEmptyInclude, Primitive<User>>("same");
+
+const userWithIdOnly = User({ select: { id: true } });
+TypeIs<typeof userWithIdOnly, Pick<Primitive<User>, "id">>("same");
+const userWithArticlesOnly = User({ select: { articles: true } });
+TypeIs<typeof userWithArticlesOnly, Record<"articles", Primitive<Article>[]>>(
+  "same"
+);
+const userWithArticles = User({ include: { articles: true } });
+TypeIs<
+  typeof userWithArticles,
+  Primitive<User> & Record<"articles", Primitive<Article>[]>
+>("same");
+
+const userNested = User({
+  include: {
+    articles: true,
+    comments: { select: { user: { select: { id: true } } } },
+  },
+});
+TypeIs<
+  typeof userNested,
+  Primitive<User> & {
+    articles: Primitive<Article>[];
+    comments: {
+      user: { id: number } | null;
+    }[];
+  }
+>("same");
